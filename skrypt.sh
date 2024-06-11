cc33ee30cfe1fed0320c23826f9d9a7966af17e2
Mateusz Grzegorowski <dsw52683@student.dsw.edu.pl>
2024-06-11 20:24:05 +0200

    logs

--- skrypt.sh
+++ skrypt.sh
@@ -3,5 +3,15 @@
 case "$1" in
 	--date)
 	   date;;
-	*)
+	--logs|-l)
+		count=${1:100}
+		for i in $(seq 1 $count); do
+		filename = "errors/error$i.txt"
+		echo Nazwa_pliku: "log$i.txt"
+		echo "Nazwa: $filename" > $filename
+		echo "Skrypt: $0 >> $filename"
+		echo "Dane: $(date)" >> $filename
+	done;;
+*)
+
 esac
